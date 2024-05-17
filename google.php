f78013cc08ae864fe242db5cb0587dda0d7ecc05
darkzed9 <darkzed1338@protonmail.com>
2024-05-17 09:08:11 -0700

    1.0.0
    add files

---
+++ google.php
@@ -0,0 +1,33 @@
+<?php
+//DOH by Darkzed
+if (isset($_SERVER['CONTENT_TYPE']) && $_SERVER['CONTENT_TYPE'] == 'application/dns-message')
+{
+        $request = file_get_contents("php://input");
+        header("Content-Type: application/dns-message");
+        $fp = fsockopen("udp://8.8.8.8", 53, $errno, $errstr);
+        if ($fp)
+        {
+                fwrite($fp, $request);
+                echo fread($fp, 4096);
+                fclose($fp);
+        }
+}
+else if (isset($_GET['dns']))
+{
+        $request = base64_decode(str_replace(array('-', '_'), array('+', '/'), $_GET['dns']));
+        header("Content-Type: application/dns-message");
+        $fp = fsockopen("udp://8.8.8.8", 53, $errno, $errstr);
+        if ($fp)
+        {
+                fwrite($fp, $request);
+                echo fread($fp, 4096);
+                fclose($fp);
+        }
+        else
+        {
+            echo $errstr;
+        }
+}else{
+        echo 'access denide';
+}
+?>
